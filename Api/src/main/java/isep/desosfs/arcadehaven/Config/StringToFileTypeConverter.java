e1d96b55d858421392dff6763f06a025d063ef35
diogojms2 <1250505@isep.ipp.pt>
2026-05-13 08:16:46 +0100

    Add FileType converter component

--- Api/src/main/java/isep/desosfs/arcadehaven/Config/StringToFileTypeConverter.java
+++ Api/src/main/java/isep/desosfs/arcadehaven/Config/StringToFileTypeConverter.java
@@ -1,7 +1,6 @@
 package isep.desosfs.arcadehaven.Config;
 
 import isep.desosfs.arcadehaven.Domain.Enums.FileType;
-import isep.desosfs.arcadehaven.Domain.Enums.Role;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
